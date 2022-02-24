d0c44cead780d7fb5cec9f6691d18772b0cc2241
KanuteGX <pimentel2002d@gmail.com>
2022-02-24 07:44:28 -0400

    daniel home

--- JS/home_page/scripts.js
+++ JS/home_page/scripts.js
@@ -46,21 +46,3 @@ setInterval(() => {
 // addEventListener('resize', () => {
 //   efectWave();
 // });
-
-
-//section estrategias
-
-const bgrCardTwo = document.getElementById('bgrCardTwo');
-const containerCardTwo = document.getElementById('containerCardTwo');
-const cardTwo = document.getElementById('cardTwo');
-
-const elementOpacityScroll = () => {
-  let scrollTop = document.firstElementChild.scrollTop;
-  let cardTop = containerCardTwo.offsetTop;
-  let viewportHeight = document.firstElementChild.clientHeight;
-
-  if (scrollTop > cardTop - viewportHeight / 1.3) {
-    bgrCardTwo.classList.add('background-card2__scroll');
-    cardTwo.classList.add('card2__scroll');
-  }
-};
