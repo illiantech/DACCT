d16b08dc63bf2e069c9ec59fb80b99ecf27f9780
pedroj4567 <pedroj4567@gmail.com>
2022-02-28 14:54:44 -0400

    Subiendo cambios

--- JS/template/contactoForm.js
+++ JS/template/contactoForm.js
@@ -1,5 +1,7 @@
 //validaciones de formulario 
 
+const { unwatchFile } = require("fs");
+
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
@@ -12,9 +14,13 @@ message.addEventListener('blur', validateName);
 
 //Funcion de validacion 
 function validateName(e){
+
     if(e.target.value.length < 1){
-        
+     
     }else{
+        e.target.style.border= '3px solid';
         console.log('paso');
     }
+
+
 }
