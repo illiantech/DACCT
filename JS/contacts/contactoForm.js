bc3ffd732d83e29f703d9da00291db7dbac23809
pedroj4567 <pedroj4567@gmail.com>
2022-03-03 11:56:03 -0400

    cambios ghots

--- JS/contacts/contactoForm.js
+++ JS/contacts/contactoForm.js
@@ -11,3 +11,16 @@ email.addEventListener('blur', validateName);
 message.addEventListener('blur', validateName);
 
 //Funcion de validacion 
+function validateName(e){
+ const countLettersInputs = e.target.value.length;
+ if(countLettersInputs < 1){
+     e.target.style.border = '2px solid red'
+ }else{
+    e.target.style.border = '2px solid green'
+ }
+
+
+
+
+
+}
