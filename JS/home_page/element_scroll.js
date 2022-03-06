9337e6ecaf5e8230516bb24d68eb7896cac30d09
KanuteGX <pimentel2002d@gmail.com>
2022-03-06 02:24:18 -0400

    pimen 6/3 2:24 am

--- JS/home_page/element_scroll.js
+++ JS/home_page/element_scroll.js
@@ -1,19 +1,39 @@
+// section blog
+const blogHome = document.getElementById('blogHome');
+const blogHomeTop = blogHome.offsetTop;
+
 //section estrategias
 
-const bgrCardTwo = document.getElementById('bgrCardTwo');
 const containerCardTwo = document.getElementById('containerCardTwo');
 const cardTwo = document.getElementById('cardTwo');
+const cardTop = containerCardTwo.offsetTop;
 
+// section programas
+const programs = document.getElementById('programs');
+const programsTop = programs.offsetTop;
+
+// arrays de elementos a aplicar este efecto
+const elements = {
+  elementOne: [blogHome, blogHomeTop, 'blog__scroll'],
+  elementTwo: [cardTwo, cardTop, 'card2__scroll'],
+  elementThree: [programs, programsTop, 'programs__scroll']
+};
+
+// para el mantenimiento optimizar estos if
 const elementOpacityScroll = () => {
   const scrollTop = document.firstElementChild.scrollTop;
-  const cardTop = containerCardTwo.offsetTop;
   const viewportHeight = document.firstElementChild.clientHeight;
-  if (scrollTop > cardTop - viewportHeight / 1.3) {
-    bgrCardTwo.classList.add('background-card2__scroll');
-    cardTwo.classList.add('card2__scroll');
+
+  for (const element in elements) {
+    if (scrollTop > elements[element][1] - viewportHeight / 2) {
+      elements[element][0].classList.add(elements[element][2]);
+
+      // se requiere cambiar manualmente la clausula
+      if (element == 'elementThree') {
+        removeEventListener('scroll', elementOpacityScroll);
+      }
+    }
   }
 };
 
-addEventListener('scroll', () => {
-  elementOpacityScroll();
-});
+addEventListener('scroll', elementOpacityScroll);
