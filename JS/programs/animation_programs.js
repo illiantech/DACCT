0888b257171313e2ff08915b9f837a0f85940078
KanuteGX <pimentel2002d@gmail.com>
2022-03-07 21:36:01 -0400

    pimen 7/3 9:35pm

---
+++ JS/programs/animation_programs.js
@@ -0,0 +1,23 @@
+// Element scroll
+const programs = document.getElementById('programs');
+
+const programsArray = Array.from(programs.children);
+
+const elementAnimationScroll = () => {
+  let scrollTop = document.firstElementChild.scrollTop;
+  let viewportHeight = document.firstElementChild.clientHeight;
+
+  programsArray.forEach((item, index) => {
+    let itemTop = item.offsetTop;
+
+    if (scrollTop > itemTop + viewportHeight / 1.7) {
+      item.classList.add('programs-list--item__scroll');
+      console.log('HOLAAAA');
+      if (index == programsArray.length - 1) {
+        removeEventListener('scroll', elementAnimationScroll);
+      }
+    }
+  });
+};
+
+addEventListener('scroll', elementAnimationScroll);
