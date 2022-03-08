228c592329d3422377b7f2c3493b0628d41ade7b
KanuteGX <pimentel2002d@gmail.com>
2022-03-08 11:34:15 -0400

    pimen

--- JS/template/load.js
+++ JS/template/load.js
@@ -1,9 +1,13 @@
 // Pantalla de carga
 
 const load = document.getElementById('load');
+const body = load.parentElement;
+
+body.style.overflowY = 'hidden';
 
 window.addEventListener('load', (e) => {
   load.classList.add('load__end');
+  body.style.overflowY = 'auto';
   setTimeout(() => {
     load.style.display = 'none';
   }, 1050);
