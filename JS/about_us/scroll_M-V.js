09b39f4c7fa47c8081a89e4d0168043e5621ae2b
KanuteGX <pimentel2002d@gmail.com>
2022-03-09 01:12:38 -0400

    pimen 9/3 1:12am

--- JS/about_us/scroll_M-V.js
+++ JS/about_us/scroll_M-V.js
@@ -4,7 +4,7 @@ const efectScrollMV = () => {
   const MVTop = misionVision.offsetTop;
   let scrollTop = document.firstElementChild.scrollTop;
   let viewportHeight = document.firstElementChild.clientHeight;
-  console.log(1);
+
   if (scrollTop > MVTop - viewportHeight / 3) {
     misionVision.classList.add('second-container__scroll');
     removeEventListener('scroll', efectScrollMV);
