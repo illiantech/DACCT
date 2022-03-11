fb54d543beb77916cf8b7d955aeb8ab005b646ab
KanuteGX <pimentel2002d@gmail.com>
2022-03-11 03:34:16 -0400

    pimen 11/3 3:34am

--- JS/blog/news__open.js
+++ JS/blog/news__open.js
@@ -1,3 +1,38 @@
 if (history.state != null) {
   console.log(history.state.validOpenBlog);
 }
+
+const blogList = document.getElementById('blogList');
+
+const blogListArray = Array.from(blogList.children);
+
+// active open blog
+
+blogListArray.forEach((item, index) => {
+  const itemButtom =
+    item.children[1].firstElementChild.lastElementChild.firstElementChild;
+
+  itemButtom.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    item.classList.add('blog--list-item__open');
+
+    setTimeout(() => {
+      item.classList.add('blog--list-item__open-opacity');
+    }, 10);
+  });
+});
+
+// desactive open blog
+
+blogListArray.forEach((item, index) => {
+  const itemButtomDesac = item.children[0];
+
+  itemButtomDesac.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    item.classList.remove('blog--list-item__open');
+
+    item.classList.remove('blog--list-item__open-opacity');
+  });
+});
