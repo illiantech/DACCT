898e50509228240ea0e469ee74b2ebd24c307226
KanuteGX <pimentel2002d@gmail.com>
2022-03-15 18:22:19 -0400

    pimen 15/3 6:22pm

--- JS/home_page/slider_alliances.js
+++ JS/home_page/slider_alliances.js
@@ -13,145 +13,165 @@
 //   }
 // }
 
-const scrollAlliance = () => {
-  //   fragment para elementos hermanos menores
-  const fragment = document.createDocumentFragment();
+// const scrollAlliance = () => {
+//   //   fragment para elementos hermanos menores
+//   const fragment = document.createDocumentFragment();
 
-  if (viewportWidth * 2 == allianceList.scrollLeft) {
-    let items = [allianceList.children[0]];
-    fragment.append(...items);
+//   if (viewportWidth * 2 == allianceList.scrollLeft) {
+//     let items = [allianceList.children[0]];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 3 == allianceList.scrollLeft) {
-    let items = [allianceList.children[0], allianceList.children[1]];
-    fragment.append(...items);
+//   if (viewportWidth * 3 == allianceList.scrollLeft) {
+//     let items = [allianceList.children[0], allianceList.children[1]];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 4 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 4 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 5 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2],
-      allianceList.children[3]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 5 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2],
+//       allianceList.children[3]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 6 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2],
-      allianceList.children[3],
-      allianceList.children[4]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 6 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2],
+//       allianceList.children[3],
+//       allianceList.children[4]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 7 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2],
-      allianceList.children[3],
-      allianceList.children[4],
-      allianceList.children[5]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 7 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2],
+//       allianceList.children[3],
+//       allianceList.children[4],
+//       allianceList.children[5]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 8 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2],
-      allianceList.children[3],
-      allianceList.children[4],
-      allianceList.children[5],
-      allianceList.children[6]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 8 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2],
+//       allianceList.children[3],
+//       allianceList.children[4],
+//       allianceList.children[5],
+//       allianceList.children[6]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  if (viewportWidth * 9 == allianceList.scrollLeft) {
-    let items = [
-      allianceList.children[0],
-      allianceList.children[1],
-      allianceList.children[2],
-      allianceList.children[3],
-      allianceList.children[4],
-      allianceList.children[5],
-      allianceList.children[6],
-      allianceList.children[7]
-    ];
-    fragment.append(...items);
+//   if (viewportWidth * 9 == allianceList.scrollLeft) {
+//     let items = [
+//       allianceList.children[0],
+//       allianceList.children[1],
+//       allianceList.children[2],
+//       allianceList.children[3],
+//       allianceList.children[4],
+//       allianceList.children[5],
+//       allianceList.children[6],
+//       allianceList.children[7]
+//     ];
+//     fragment.append(...items);
 
-    allianceList.append(fragment);
-    allianceList.scrollLeft = viewportWidth;
-  }
+//     allianceList.append(fragment);
+//     allianceList.scrollLeft = viewportWidth;
+//   }
 
-  // RESEST slider auto mobile
-  sliderAutoDesac();
-};
+//   // RESEST slider auto mobile
+//   sliderAutoDesac();
+// };
 
-const resizeAlliance = (viewportWidth) => {
-  // ancho de viewport
+// const resizeAlliance = (viewportWidth) => {
+//   // ancho de viewport
 
-  // para tener el ancho del documento y verificar (tomando en cuenta que los elementos a scrollear son equivalentes al viewport) que el scroll realizado sea N veces el viewportWidth y asi saber cuanto elementos existen a la izquierda
+//   // para tener el ancho del documento y verificar (tomando en cuenta que los elementos a scrollear son equivalentes al viewport) que el scroll realizado sea N veces el viewportWidth y asi saber cuanto elementos existen a la izquierda
 
-  // desplazar un elemento para que quede otro a la izquierda
-  allianceList.scrollLeft = viewportWidth;
+//   // desplazar un elemento para que quede otro a la izquierda
+//   allianceList.scrollLeft = viewportWidth;
 
-  // Event scroll function
-  allianceList.addEventListener('scroll', scrollAlliance);
+//   // Event scroll function
+//   allianceList.addEventListener('scroll', scrollAlliance);
 
-  // activar slider auto mobile
-  sliderAutoActive(viewportWidth);
-};
+//   // activar slider auto mobile
+//   sliderAutoActive(viewportWidth);
+// };
 
-// slider automaitco Mobile en scope General - ATTENTION
+// slider CSS MOBILE
+
+const allianceList = document.getElementById('allianceList');
+
 let sliderAutoMobile;
-// desplazaiento de un elemento a otro reposicionante con primer IF
-const sliderAutoActive = (viewportWidth) => {
-  sliderAutoMobile = setInterval(() => {
-    allianceList.scrollLeft = viewportWidth * 2;
-  }, 6000);
+
+const sliderAutoActive = (viewportWidth, index) => {
+  sliderAutoMobile = setTimeout(() => {
+    // NEW
+
+    if (index <= allianceList.children.length - 1) {
+      allianceList.scrollLeft = viewportWidth * index;
+
+      index++;
+
+      sliderAutoActive(viewportWidth, index);
+    } else {
+      index = 0;
+      sliderAutoActive(viewportWidth, index);
+    }
+  }, 5000);
 };
 
 const sliderAutoDesac = () => {
   clearInterval(sliderAutoMobile);
-  sliderAutoActive(viewportWidth);
+
+  sliderAutoActive(viewportWidth, 0);
 };
+
+allianceList.addEventListener('touchmove', () => {
+  //NEW
+  sliderAutoDesac();
+});
 
 // exit slider auto mobile
 
@@ -165,20 +185,22 @@ const sliderAuto = () => {
   }, 10000);
 };
 
-sliderAuto();
-
 // aplicacion de slider CSS con JS
-
-const allianceList = document.getElementById('allianceList');
 
 // construccion de slider infinito CSS mobile
 let viewportWidth = document.firstElementChild.clientWidth;
 
+let validSliderMovil = true;
+
 if (viewportWidth < 1000) {
-  // limpiar TIEMPOS de slider pc
-  clearTimeout(sliderAutoTime);
-  clearInterval(sliderAutoInterval);
-  resizeAlliance(viewportWidth);
+  // activar slider auto mobile // NEW
+
+  if (validSliderMovil) {
+    sliderAutoActive(viewportWidth, 1); // NEW
+    validSliderMovil = false;
+  }
+} else {
+  sliderAuto();
 }
 
 addEventListener('resize', () => {
@@ -189,10 +211,21 @@ addEventListener('resize', () => {
     // limpiar TIEMPOS de slider pc
     clearTimeout(sliderAutoTime);
     clearInterval(sliderAutoInterval);
-    // limpiar acumulacion de automatic slider mobile
-    clearInterval(sliderAutoMobile);
-    allianceList.removeEventListener('scroll', scrollAlliance);
-    resizeAlliance(viewportWidth);
+    //     // limpiar acumulacion de automatic slider mobile
+    //     clearInterval(sliderAutoMobile);
+    //     allianceList.removeEventListener('scroll', scrollAlliance);
+    //     resizeAlliance(viewportWidth);
+
+    if (validSliderMovil) {
+      sliderAutoActive(viewportWidth, 1); // NEW
+      validSliderMovil = false;
+      sliderAutoDesac();
+    }
+  } else {
+    if (!validSliderMovil) {
+      clearInterval(sliderAutoMobile); // NEW
+      validSliderMovil = true;
+    }
   }
 });
 
