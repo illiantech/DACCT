5d2174a22bdb7905e355292e59c3d3c0e6d723d7
KanuteGX <pimentel2002d@gmail.com>
2022-03-22 00:50:25 -0400

    pimen 22/3 12:50am

--- JS/template/lang.js
+++ JS/template/lang.js
@@ -1,5 +1,35 @@
+// ruta de lang
+
+const routeLang = document.querySelector('[data-routeLang]').dataset.routelang;
+
+// declarion de los elementos al traducir
+
+const blocksContent = document.querySelectorAll('[data-section]');
+
+// funcion async - await fetch JSON Lang
+
+const translate = async (lang) => {
+  // objeto JSON
+  const objectTranslate = await fetch(`../../JSON/${routeLang}/${lang}.json`).then(
+    (res) => res.json()
+  );
+
+  // iteracion de bloques de contenido para agrear datos de objeto a partir del indice del mismo
+  for (const blockContent of blocksContent) {
+    const section = blockContent.dataset.section;
+    const content = blockContent.dataset.content;
+
+    blockContent.innerHTML = objectTranslate[section][content];
+  }
+};
+
+// declaracion del click para cambio de idioma
+
 const languageContainer = document.getElementById('languageContainer');
 
 languageContainer.addEventListener('click', (e) => {
-  console.log(e.target.dataset.lang);
+  const language = e.target.dataset.lang;
+  if (language != undefined) {
+    translate(language);
+  }
 });
