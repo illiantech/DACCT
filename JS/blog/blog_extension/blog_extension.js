d31cfe14702e1c1578faaca53d5854f07d0fbe52
KanuteGX <pimentel2002d@gmail.com>
2022-03-26 13:34:38 -0400

    pimen 26/3 01:34 pm

--- JS/blog/blog_extension/blog_extension.js
+++ JS/blog/blog_extension/blog_extension.js
@@ -1,8 +1,84 @@
+// declaracion de elementos (input y container)
+
+const searchBar = document.getElementById('searchBar');
+
+const containerList = document.getElementById('containerList');
+
+// eliminarDiacriticosEs
+
+function removeAccents(texto) {
+	return texto
+		.normalize('NFD')
+		.replace(
+			/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+			'$1'
+		)
+		.normalize();
+}
+
+// filtro de busqueda
+
+let articles;
+
+const filterSearch = () => {
+	// declaracion de elementos
+	let valueLowerCase = searchBar.value.toLowerCase(); //toma el valor el input y lo pasa a minuscula
+	const text = removeAccents(valueLowerCase);
+
+	const list = document.createElement('ul');
+	list.classList.add('list-blog');
+
+	// busqueda de articulos
+	for (const article of articles) {
+		const title = removeAccents(article.title.toLowerCase());
+
+		if (title.includes(text)) {
+			// crecion de elementos
+			const item = document.createElement('li');
+			item.classList.add('list-blog--item');
+
+			const link = document.createElement('a');
+			link.classList.add('list-blog--item-title');
+
+			link.textContent = article.title;
+			link.title = 'Abrir artículo';
+			link.href = article.link;
+			link.target = '_blank';
+
+			const date = document.createElement('time');
+			date.classList.add('list-blog--item-time');
+
+			date.textContent = article.visual_date;
+			date.dateTime = article.date;
+
+			item.append(link, date);
+			// esto con la finalidad de insertar una vez en el doom
+			list.append(item);
+		}
+	}
+
+	// Insercion de elementos en el DOM con metodologia fragment
+	if (containerList.children.length > 0)
+		containerList.removeChild(containerList.children[0]);
+
+	// aplicar estilos de invalid a barra de busqueda
+	searchBar.classList.toggle('search-bar__invalid', list.children.length === 0);
+
+	if (list.children.length === 0) {
+		// en caso de que no ninguna key coinsida
+		const item = document.createElement('li');
+		item.textContent = 'Articulo no encontrado...';
+		item.classList.add('list-blog--item__undefined');
+		// agregar alerta en lista
+		list.append(item);
+
+		containerList.append(list);
+	} else containerList.append(list);
+};
+
 // Extraccion de articulos de API JSON con AJAX
 
 const fetchArticles = async (lang) => {
-	let articles;
-
 	if (lang) {
 		articles = await fetch(
 			`https://kanutegx.github.io/DACCT/JSON/register_blog/${lang}.json`
@@ -17,78 +93,6 @@ const fetchArticles = async (lang) => {
 			.then((res) => res.articles);
 	}
 
-	const searchBar = document.getElementById('searchBar');
-
-	const containerList = document.getElementById('containerList');
-
-	// eliminarDiacriticosEs
-
-	function removeAccents(texto) {
-		return texto
-			.normalize('NFD')
-			.replace(
-				/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
-				'$1'
-			)
-			.normalize();
-	}
-
-	const filterSearch = () => {
-		// declaracion de elementos
-		let valueLowerCase = searchBar.value.toLowerCase(); //toma el valor el input y lo pasa a minuscula
-		const text = removeAccents(valueLowerCase);
-
-		const list = document.createElement('ul');
-		list.classList.add('list-blog');
-
-		// busqueda de articulos
-		for (const article of articles) {
-			const title = removeAccents(article.title.toLowerCase());
-
-			if (title.includes(text)) {
-				// crecion de elementos
-				const item = document.createElement('li');
-				item.classList.add('list-blog--item');
-
-				const link = document.createElement('a');
-				link.classList.add('list-blog--item-title');
-
-				link.textContent = article.title;
-				link.title = 'Abrir artículo';
-				link.href = article.link;
-				link.target = '_blank';
-
-				const date = document.createElement('time');
-				date.classList.add('list-blog--item-time');
-
-				date.textContent = article.visual_date;
-				date.dateTime = article.date;
-
-				item.append(link, date);
-				// esto con la finalidad de insertar una vez en el doom
-				list.append(item);
-			}
-		}
-
-		// Insercion de elementos en el DOM con metodologia fragment
-		if (containerList.children.length > 0)
-			containerList.removeChild(containerList.children[0]);
-
-		// aplicar estilos de invalid a barra de busqueda
-		searchBar.classList.toggle('search-bar__invalid', list.children.length === 0);
-
-		if (list.children.length === 0) {
-			// en caso de que no ninguna key coinsida
-			const item = document.createElement('li');
-			item.textContent = 'Articulo no encontrado...';
-			item.classList.add('list-blog--item__undefined');
-			// agregar alerta en lista
-			list.append(item);
-
-			containerList.append(list);
-		} else containerList.append(list);
-	};
-
 	searchBar.addEventListener('keyup', filterSearch);
 
 	filterSearch();
@@ -100,6 +104,8 @@ languageContainer.addEventListener('click', (e) => {
 	const language = e.target.dataset.lang;
 
 	if (language != undefined) {
+		// limpiar evento para que no se sobre sature al cambiar el idioma
+		searchBar.removeEventListener('keyup', filterSearch);
 		fetchArticles(language);
 	}
 });
