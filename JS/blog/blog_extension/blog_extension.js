f3a1bbd190f6dbd1204d8ce0bdd996e03dc5638f
KanuteGX <pimentel2002d@gmail.com>
2022-03-26 12:48:44 -0400

    pimen 26/3 12:00 am

--- JS/blog/blog_extension/blog_extension.js
+++ JS/blog/blog_extension/blog_extension.js
@@ -1,83 +1,107 @@
-const fetchArticles = async () => {
-  return await fetch(
-    `https://kanutegx.github.io/DACCT/JSON/register_blog/es.json`
-  ).then((res) => res.json().articles);
+// Extraccion de articulos de API JSON con AJAX
+
+const fetchArticles = async (lang) => {
+	let articles;
+
+	if (lang) {
+		articles = await fetch(
+			`https://kanutegx.github.io/DACCT/JSON/register_blog/${lang}.json`
+		)
+			.then((res) => res.json())
+			.then((res) => res.articles);
+	} else {
+		articles = await fetch(
+			`https://kanutegx.github.io/DACCT/JSON/register_blog/en.json`
+		)
+			.then((res) => res.json())
+			.then((res) => res.articles);
+	}
+
+	const searchBar = document.getElementById('searchBar');
+
+	const containerList = document.getElementById('containerList');
+
+	// eliminarDiacriticosEs
+
+	function removeAccents(texto) {
+		return texto
+			.normalize('NFD')
+			.replace(
+				/([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
+				'$1'
+			)
+			.normalize();
+	}
+
+	const filterSearch = () => {
+		// declaracion de elementos
+		let valueLowerCase = searchBar.value.toLowerCase(); //toma el valor el input y lo pasa a minuscula
+		const text = removeAccents(valueLowerCase);
+
+		const list = document.createElement('ul');
+		list.classList.add('list-blog');
+
+		// busqueda de articulos
+		for (const article of articles) {
+			const title = removeAccents(article.title.toLowerCase());
+
+			if (title.includes(text)) {
+				// crecion de elementos
+				const item = document.createElement('li');
+				item.classList.add('list-blog--item');
+
+				const link = document.createElement('a');
+				link.classList.add('list-blog--item-title');
+
+				link.textContent = article.title;
+				link.title = 'Abrir artículo';
+				link.href = article.link;
+				link.target = '_blank';
+
+				const date = document.createElement('time');
+				date.classList.add('list-blog--item-time');
+
+				date.textContent = article.visual_date;
+				date.dateTime = article.date;
+
+				item.append(link, date);
+				// esto con la finalidad de insertar una vez en el doom
+				list.append(item);
+			}
+		}
+
+		// Insercion de elementos en el DOM con metodologia fragment
+		if (containerList.children.length > 0)
+			containerList.removeChild(containerList.children[0]);
+
+		// aplicar estilos de invalid a barra de busqueda
+		searchBar.classList.toggle('search-bar__invalid', list.children.length === 0);
+
+		if (list.children.length === 0) {
+			// en caso de que no ninguna key coinsida
+			const item = document.createElement('li');
+			item.textContent = 'Articulo no encontrado...';
+			item.classList.add('list-blog--item__undefined');
+			// agregar alerta en lista
+			list.append(item);
+
+			containerList.append(list);
+		} else containerList.append(list);
+	};
+
+	searchBar.addEventListener('keyup', filterSearch);
+
+	filterSearch();
 };
 
-const articles = fetchArticles();
+fetchArticles(localStorage.getItem('lang'));
 
-const searchBar = document.getElementById('searchBar');
+const languageContainer = document.getElementById('languageContainer');
 
-const containerList = document.getElementById('containerList');
+languageContainer.addEventListener('click', (e) => {
+	const language = e.target.dataset.lang;
 
-// eliminarDiacriticosEs
-
-function removeAccents(texto) {
-  return texto
-    .normalize('NFD')
-    .replace(
-      /([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+/gi,
-      '$1'
-    )
-    .normalize();
-}
-
-const filterSearch = () => {
-  // declaracion de elementos
-  let valueLowerCase = searchBar.value.toLowerCase(); //toma el valor el input y lo pasa a minuscula
-  const text = removeAccents(valueLowerCase);
-
-  const list = document.createElement('ul');
-  list.classList.add('list-blog');
-
-  // busqueda de articulos
-  for (const article of articles) {
-    const title = removeAccents(article.title.toLowerCase());
-
-    if (title.includes(text)) {
-      // crecion de elementos
-      const item = document.createElement('li');
-      item.classList.add('list-blog--item');
-
-      const link = document.createElement('a');
-      link.classList.add('list-blog--item-title');
-
-      link.textContent = article.title;
-      link.title = 'Abrir artículo';
-      link.href = article.link;
-      link.target = '_blank';
-
-      const date = document.createElement('time');
-      date.classList.add('list-blog--item-time');
-
-      date.textContent = article.visual_date;
-      date.dateTime = article.date;
-
-      item.append(link, date);
-      // esto con la finalidad de insertar una vez en el doom
-      list.append(item);
-    }
-  }
-
-  // Insercion de elementos en el DOM con metodologia fragment
-  if (containerList.children.length > 0)
-    containerList.removeChild(containerList.children[0]);
-
-  // aplicar estilos de invalid a barra de busqueda
-  searchBar.classList.toggle('search-bar__invalid', list.children.length === 0);
-
-  if (list.children.length === 0) {
-    // en caso de que no ninguna key coinsida
-    const item = document.createElement('li');
-    item.textContent = 'Articulo no encontrado...';
-    item.classList.add('list-blog--item__undefined');
-    // agregar alerta en lista
-    list.append(item);
-
-    containerList.append(list);
-  } else containerList.append(list);
-};
-
-searchBar.addEventListener('keyup', filterSearch);
-
-filterSearch();
+	if (language != undefined) {
+		fetchArticles(language);
+	}
+});
--- JS/template/lang.js
+++ JS/template/lang.js
@@ -9,42 +9,42 @@ const blocksContent = document.querySelectorAll('[data-section]');
 // funcion async - await fetch JSON Lang
 
 const translate = async (lang) => {
-  // objeto JSON
-  // la ruta del fetch esta sujeta a cambio en funcion al hosting
-  const objectTranslate = await fetch(
-    `https://kanutegx.github.io/DACCT/JSON/${routeLang}/${lang}.json`
-  ).then((res) => res.json());
+	// objeto JSON
+	// la ruta del fetch esta sujeta a cambio en funcion al hosting
+	const objectTranslate = await fetch(
+		`https://kanutegx.github.io/DACCT/JSON/${routeLang}/${lang}.json`
+	).then((res) => res.json());
 
-  // iteracion de bloques de contenido para agregar datos del objeto a partir del indice del mismo
-  for (const blockContent of blocksContent) {
-    const section = blockContent.dataset.section;
-    const content = blockContent.dataset.content;
+	// iteracion de bloques de contenido para agregar datos del objeto a partir del indice del mismo
+	for (const blockContent of blocksContent) {
+		const section = blockContent.dataset.section;
+		const content = blockContent.dataset.content;
 
-    if (blockContent.hasAttribute('alt'))
-      // colocar en los alts de las imagenes la traduccion
-      blockContent.alt = objectTranslate[section][content];
-    else if (blockContent.hasAttribute('placeholder'))
-      // colocar placeholder para los inputs
-      blockContent.placeholder = objectTranslate[section][content];
-    else if (blockContent.hasAttribute('title') && blockContent.hasChildNodes()) {
-      // colocar en titles con nodos hijos
-      if (blockContent.firstChild.nodeName == '#text') {
-        // colocar en titles con nodos hijos de tipo text
-        const textFormat = blockContent.firstChild.textContent.trim();
-        if (textFormat.length == 0)
-          // colocar en titles con nodos hijos de tipo text con 0 caracteres despues de formateo
-          blockContent.title = objectTranslate[section][content];
-        else {
-          // colocar en titles con nodos hijos de tipo text con MAS de 0 caracteres despues de formateo
-          blockContent.title = objectTranslate[section][content];
-          blockContent.innerHTML = objectTranslate[section][content];
-        }
-      } else blockContent.title = objectTranslate[section][content]; // colocar en titles con nodos hijos que NO SEAN de tipo text
-    } else if (blockContent.hasAttribute('title') && !blockContent.hasChildNodes())
-      // colocar en titles que NO tengan nodos hijos
-      blockContent.title = objectTranslate[section][content];
-    else blockContent.innerHTML = objectTranslate[section][content]; // colocar en cualquier texto que desee traducir
-  }
+		if (blockContent.hasAttribute('alt'))
+			// colocar en los alts de las imagenes la traduccion
+			blockContent.alt = objectTranslate[section][content];
+		else if (blockContent.hasAttribute('placeholder'))
+			// colocar placeholder para los inputs
+			blockContent.placeholder = objectTranslate[section][content];
+		else if (blockContent.hasAttribute('title') && blockContent.hasChildNodes()) {
+			// colocar en titles con nodos hijos
+			if (blockContent.firstChild.nodeName == '#text') {
+				// colocar en titles con nodos hijos de tipo text
+				const textFormat = blockContent.firstChild.textContent.trim();
+				if (textFormat.length == 0)
+					// colocar en titles con nodos hijos de tipo text con 0 caracteres despues de formateo
+					blockContent.title = objectTranslate[section][content];
+				else {
+					// colocar en titles con nodos hijos de tipo text con MAS de 0 caracteres despues de formateo
+					blockContent.title = objectTranslate[section][content];
+					blockContent.innerHTML = objectTranslate[section][content];
+				}
+			} else blockContent.title = objectTranslate[section][content]; // colocar en titles con nodos hijos que NO SEAN de tipo text
+		} else if (blockContent.hasAttribute('title') && !blockContent.hasChildNodes())
+			// colocar en titles que NO tengan nodos hijos
+			blockContent.title = objectTranslate[section][content];
+		else blockContent.innerHTML = objectTranslate[section][content]; // colocar en cualquier texto que desee traducir
+	}
 };
 
 //  localStorage para indicar la heredacion del cambio de idioma en otras paginas cuando recargue y cambio de paginación
@@ -52,19 +52,19 @@ const translate = async (lang) => {
 
 if (localStorage.getItem('lang') === 'es') translate(localStorage.getItem('lang'));
 else if (localStorage.getItem('lang') === 'en')
-  translate(localStorage.getItem('lang'));
+	translate(localStorage.getItem('lang'));
 
 // declaracion del click para cambio de idioma
 
 const languageContainer = document.getElementById('languageContainer');
 
 languageContainer.addEventListener('click', (e) => {
-  const language = e.target.dataset.lang;
+	const language = e.target.dataset.lang;
 
-  if (language != undefined) {
-    translate(language);
-    localStorage.setItem('lang', `${language}`);
-  }
+	if (language != undefined) {
+		translate(language);
+		localStorage.setItem('lang', `${language}`);
+	}
 });
 
 // localStorage.clear();
