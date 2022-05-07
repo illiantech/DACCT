b6b4b7e59c46b9557c1f95074a319890d35bd4c3
KanuteGX <pimentel2002d@gmail.com>
2022-05-07 02:54:17 -0400

    pimen 07/05/2022 2:53am -cambios en elementos de slider en home page y team work en about us-

--- JS/home_page/slider_alliances.js
+++ JS/home_page/slider_alliances.js
@@ -1,93 +1,6 @@
-// slider CSS MOBILE
-
-const allianceList = document.getElementById('allianceList');
-
-let sliderAutoMobile;
-
-const sliderAutoActive = (viewportWidth, index) => {
-	sliderAutoMobile = setTimeout(() => {
-		// NEW
-
-		if (index <= allianceList.children.length - 1) {
-			allianceList.scrollLeft = viewportWidth * index;
-
-			index++;
-
-			sliderAutoActive(viewportWidth, index);
-		} else {
-			index = 0;
-			sliderAutoActive(viewportWidth, index);
-		}
-	}, 5000);
-};
-
-const sliderAutoDesac = () => {
-	clearInterval(sliderAutoMobile);
-	sliderAutoActive(viewportWidth, 0);
-};
-
-allianceList.addEventListener('touchmove', () => {
-	//NEW
-	sliderAutoDesac();
-});
-
-// exit slider auto mobile
-
-// variables slider auto PC - (se tiene arriba para que lo limpie en mobile) -----------
-let sliderAutoTime, sliderAutoInterval;
-const sliderAuto = () => {
-	sliderAutoTime = setTimeout(() => {
-		sliderAutoInterval = setInterval(() => {
-			next();
-		}, 4000);
-	}, 10000);
-};
-
-// aplicacion de slider CSS con JS
-
-// construccion de slider infinito CSS mobile
-let viewportWidth = document.firstElementChild.clientWidth;
-
-let validSliderMovil = true;
-
-if (viewportWidth < 1000) {
-	// activar slider auto mobile // NEW
-
-	if (validSliderMovil) {
-		sliderAutoActive(viewportWidth, 1); // NEW
-		validSliderMovil = false;
-	}
-} else {
-	sliderAuto();
-}
-
-addEventListener('resize', () => {
-	viewportWidth = document.firstElementChild.clientWidth;
-
-	// no se ocupara espacio en memoria cada vez que haga resize en PC
-	if (viewportWidth < 1000) {
-		// limpiar TIEMPOS de slider pc
-		clearTimeout(sliderAutoTime);
-		clearInterval(sliderAutoInterval);
-
-		if (validSliderMovil) {
-			sliderAutoActive(viewportWidth, 0); // NEW
-			validSliderMovil = false;
-			sliderAutoDesac();
-		}
-	} else {
-		if (!validSliderMovil) {
-			clearInterval(sliderAutoMobile); // NEW
-			validSliderMovil = true;
-		}
-	}
-});
-
-// INCISO -------------------------------------------------------------------------
-
 // Slider para PC
 
-// valid click event
+// valid click event (para no accionar el evento en medio de la animación)
 let validSlider = true;
 
 // prev item
@@ -113,6 +26,41 @@ const next = () => {
 };
 
 // slider automatic
+
+let sliderAutoTime, sliderAutoInterval;
+const sliderAuto = () => {
+	sliderAutoTime = setTimeout(() => {
+		sliderAutoInterval = setInterval(() => {
+			next();
+		}, 4000);
+	}, 10000);
+};
+
+// aplication slider automatic in pc and avoid in mobile
+
+let validSliderAuto = true;
+
+const ApliSliderAutoDevice = () => {
+	const viewportWidthDocument = document.firstElementChild.offsetWidth;
+
+	if (viewportWidthDocument < 1000) {
+		clearTimeout(sliderAutoTime);
+		clearInterval(sliderAutoInterval);
+		validSliderAuto = true;
+	} else if (validSliderAuto) {
+		sliderAuto();
+		// cuando esta en "true" entra y se coloca en "false" para evitar que se vuelva a ejecutar el evento "resize"
+		validSliderAuto = false;
+	}
+};
+
+ApliSliderAutoDevice();
+
+// aplication slider automatic in pc and avoid in mobile - dynamically
+
+addEventListener('resize', ApliSliderAutoDevice);
+
+// slider automatic reset
 
 const sliderAutoReset = () => {
 	clearTimeout(sliderAutoTime);
